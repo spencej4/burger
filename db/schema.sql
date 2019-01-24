db358f44442360e0ac025f02f159ee57e6667c12
Spencer Jack <spencer.jack@alaskaair.com>
2019-01-24 12:34:48 -0800

    adds working app

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger varchar(255) NOT NULL,
+	/* original */
+	-- devoured BOOLEAN NOT NULL,
+	/* new */
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+)
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger, devoured) VALUES ('Honeybacon cheeseburger', false);
+INSERT INTO burgers (burger, devoured) VALUES ('Spicy chicken bbq mesquite', false);
+INSERT INTO burgers (burger, devoured) VALUES ('Double beef patty with lettuce', false);
